906061d164046343dbea31a6d642248ef1fe8a4c
AmandaAzevedo <amanda.azevedo@dcx.ufpb.br>
2021-11-18 16:49:52 -0300

    Adicionado migração para inserir valores iniciais na tabela de descritores

---
+++ src/main/resources/db/migration/data/V20211118164543__create_desriptors.sql
@@ -0,0 +1,4 @@
+INSERT INTO descriptor_table(name) VALUES ('curso de computacao');
+INSERT INTO descriptor_table(name) VALUES ('roupa de frio');
+INSERT INTO descriptor_table(name) VALUES ('cobertor');
+INSERT INTO descriptor_table(name) VALUES ('cadeira de rodas');
